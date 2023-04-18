e657a7719e42a7b35c8246c3d52da8807f7308d4
Mahir Patel <mahir1010@gmail.com>
2023-04-18 09:33:37 -0400

    added docstrings

--- cvkit/pose_estimation/skeleton.py
+++ cvkit/pose_estimation/skeleton.py
@@ -8,49 +8,140 @@ from cvkit import MAGIC_NUMBER
 class Part(np.ndarray):
 
     def __new__(cls, arr, name, likelihood):
+        """
+        Represents a body part of the tracked subject.
+        :param arr: Array of N values defining the position in N-dimensional space
+        :type arr: list,:class:'numpy.ndarray'
+        :param name: Name of the body part
+        :type name: string
+        :param likelihood: A value indicating confidence in the accuracy of the position defined by arr
+        :type likelihood: float
+        """
         obj = np.asarray(arr).view(cls)
+        assert obj.ndim==1
         obj.name = name
         obj.likelihood = likelihood
         return obj
 
     def distance(self, obj):
+        assert len(obj)==len(self)
+        """
+        Computes distance between two Parts.
+        :param obj: N-dimensional vector
+        :type obj: :class:'cvkit.pose_estimation.Part',:class:'numpy.ndarray'
+        :return: Distance between the part and the target vector
+        :rtype: float
+        """
         return sqrt(np.sum(np.square(np.subtract(obj, self))))
 
     def magnitude(self):
+        """
+        Computes the magnitude of the Part.
+        :return: Magnitude of the Part.
+        :rtype: float
+        """
         return sqrt(np.sum(np.square(self)))
 
     def __lt__(self, other: float):
+        """
+        Checks if the likelihood of the Part is lesser than a certain value
+        :param other: A number for comparison
+        :type other: float
+        :return: Whether the likelihood is lesser than the value.
+        :rtype: bool
+        """
         return self.likelihood < other
 
     def __gt__(self, other: float):
+        """
+        Checks if the likelihood of the Part is greater than a certain value
+        :param other: A number for comparison
+        :type other: float
+        :return: Whether the likelihood is greater than the value.
+        :rtype: bool
+        """
         return self.likelihood > other
 
     def __ge__(self, other: float):
+        """
+        Checks if the likelihood of the Part is greater than or equal to a certain value
+        :param other: A number for comparison
+        :type other: float
+        :return: Whether the likelihood is greater than or equal to the value.
+        :rtype: bool
+        """
         return self.likelihood >= other
 
     def __le__(self, other: float):
+        """
+        Checks if the likelihood of the Part is lesser than or equal to a certain value
+        :param other: A number for comparison
+        :type other: float
+        :return: Whether the likelihood is lesser than or equal to the value.
+        :rtype: bool
+        """
         return self.likelihood <= other
 
     def __add__(self, other):
+        """
+        Adds an N-dimensional vector or a scalar and creates a new Part Object
+        :param other: N-dimensional vector
+        :type other: :class:`numpy.ndarray', list
+        :return: A new Part created from resulting vector.
+        :rtype: :class:`cvkit.pose_estimation.Part'
+        """
         return Part(super().__add__(other), self.name, self.likelihood)
 
     def __sub__(self, other):
+        """
+        Subtracts an N-dimensional vector or a scalar and creates a new Part Object
+        :param other: N-dimensional vector
+        :type other: :class:`numpy.ndarray', list
+        :return: A new Part created from resulting vector.
+        :rtype: :class:`cvkit.pose_estimation.Part'
+        """
         return Part(super().__sub__(other), self.name, self.likelihood)
 
     def __mul__(self, other):
+        """
+        Multiplies an N-dimensional vector or a scalar and creates a new Part Object
+        :param other: N-dimensional vector
+        :type other: :class:`numpy.ndarray', list, float
+        :return: A new Part created from resulting vector.
+        :rtype: :class:`cvkit.pose_estimation.Part'
+        """
         return Part(super().__mul__(other), self.name, self.likelihood)
 
     def __radd__(self, other):
+        """
+        Adds an N-dimensional vector or a scalar and creates a new Part Object
+        :param other: N-dimensional vector
+        :type other: :class:`numpy.ndarray', list, float
+        :return: A new Part created from resulting vector.
+        :rtype: :class:`cvkit.pose_estimation.Part'
+        """
         return Part(super().__radd__(other), self.name, self.likelihood)
 
     def numpy(self):
+        """
+        Creates a numpy array from Part
+        :return: An N-Dimensional numpy array
+        :rtype: :class:'numpy.ndarray'
+        """
         return np.array(self)
 
 
 class Skeleton:
 
     def __init__(self, body_parts: list, part_map: dict = None, likelihood_map: dict = None, behaviour=[], dims=3):
+        """
 
+        :param body_parts:
+        :param part_map:
+        :param likelihood_map:
+        :param behaviour:
+        :param dims:
+        """
         self.body_parts = body_parts
         self.body_parts_map = {}
         candidates = body_parts.copy()
