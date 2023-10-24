b7356c7476b5510476b088a9f9e176586d472c14
Mahir Patel <mahir1010@gmail.com>
2023-10-24 12:17:38 -0400

    Changed velocity filter's structure so it computes velocity real-time instead of accepting a datastore with pre-computed velocities.

--- cvkit/pose_estimation/processors/filter/velocity_filter.py
+++ cvkit/pose_estimation/processors/filter/velocity_filter.py
@@ -1,4 +1,5 @@
-from cvkit.pose_estimation.data_readers import initialize_datastore_reader
+import numpy as np
+
 from cvkit.pose_estimation.processors.processor_interface import Processor, ProcessorMetaData
 from cvkit.pose_estimation.utils import magnitude
 
@@ -7,32 +8,43 @@ class VelocityFilter(Processor):
     REQUIRES_STATS = True
     PROCESSOR_NAME = "Velocity Filter"
     PROCESSOR_ID = "cvkit_velocity_filter"
-    META_DATA = {
-        'velocity_ds_dict': ProcessorMetaData('Velocity Data Store', ProcessorMetaData.DATA_STORE, serialize=False),
-        'global_config': ProcessorMetaData('Global Config', ProcessorMetaData.GLOBAL_CONFIG),
-        'threshold_velocity': ProcessorMetaData('Velocity Threshold', ProcessorMetaData.FLOAT)}
+    META_DATA = {'target_column': ProcessorMetaData('Target Part', ProcessorMetaData.BODY_PART),
+                 'threshold': ProcessorMetaData('likelihood Threshold', ProcessorMetaData.FLOAT),
+                 'threshold_velocity': ProcessorMetaData('Velocity Threshold', ProcessorMetaData.FLOAT),
+                 'framerate': ProcessorMetaData('Framerate', ProcessorMetaData.FLOAT)}
     PROCESSOR_SUMMARY = "Filters body parts with velocity higher than the threshold."
+    DISTRIBUTED = True
 
-    def __init__(self, global_config, velocity_ds_dict, threshold_velocity):
+    def __init__(self, target_column, threshold, framerate, threshold_velocity):
         super(VelocityFilter, self).__init__()
-        self.global_config = global_config
-        self.velocity_ds_dict = velocity_ds_dict
-        self._velocity_data_store = initialize_datastore_reader(self.global_config.body_parts,
-                                                                self.velocity_ds_dict['path'],
-                                                                self.velocity_ds_dict['type'])
+        self.target_column = target_column
+        self.threshold = threshold
+        self.framerate = framerate
+        self.dt = 1 / framerate
         self.threshold_velocity = threshold_velocity
 
     def process(self, data_store):
         self._data_store = data_store
         self._data_ready = False
         self._progress = 0
-        for index in range(len(data_store)):
+        self._removed = 0
+        previous_point = None
+        previous_index = -1
+        for index, point in self._data_store.part_iterator(self.target_column):
             self._progress = int(index / len(self._data_store) * 100)
             if self.PRINT and self._progress % 10 == 0:
                 print(f'\r {self.PROCESSOR_NAME} {self._progress}% complete', end='')
-            for body_part in data_store.body_parts:
-                if magnitude(self._velocity_data_store.get_part(index, body_part)) > self.threshold_velocity:
-                    data_store.delete_part(index, body_part, True)
+            if point > self.threshold:
+                if previous_point is not None:
+                    velocity = np.subtract(point, previous_point) / ((index - previous_index) * self.dt)
+                else:
+                    velocity = [0, 0, 0]
+                if magnitude(velocity) <= self.threshold_velocity:
+                    previous_point = point.copy()
+                    previous_index = index
+                else:
+                    data_store.delete_part(index, self.target_column, True)
+                    self._removed+=1
         if self.PRINT and self._progress % 10 == 0:
             print(f'\r {self.PROCESSOR_NAME} {self._progress}% complete', end='')
         self._data_ready = True
